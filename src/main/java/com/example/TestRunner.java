e78d2097c0fb7fe6204a6e8744c44c3b7597eb5b
Sarath <getsarathg@gmail.com>
2016-03-30 10:35:15 -0500

    added a test runner main

---
+++ src/main/java/com/example/TestRunner.java
@@ -0,0 +1,10 @@
+package main.java.com.example;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Running a test method");
+	}
+
+}
